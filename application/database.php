227321867316c0843c4139e1856e50bd134f68eb
root <root@AY1212040339276a9f268.(none)>
2017-01-23 11:20:20 +0800

    up

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-    'database'       => 'dede',
+    'database'       => 'qtdhbk',
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => '',
+    'password'       => '13520651050',
     // 端口
     'hostport'       => '',
     // 连接dsn
