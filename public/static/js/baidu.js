8b9e0c77385e1f5db37efbfd3b12c89c8da71d2f
zhaoning <2005znwy@163.com>
2017-02-04 10:00:50 +0800

    up

---
+++ public/static/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9f3ca293efe89ce3f2ad8daf03b3243f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
